7e9b8ac80d801709d59045bd8c668b7256d4449a
felippefernandes <felippefernandes10@yahoo.com.br>
2014-11-14 17:42:08 -0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+//int op = 0;
+
+
+	//printf("\n\n-------MENU-------\n\n");
+	//printf("Insira sua opção:\n\n");
+	//printf("1 - Soma\n");
+	//printf("2 - Subtracao\n");
+	//scanf("%d", &op);
+
+	//switch (op){
+
+	//case 1:
+
+		return divisao(20,5);
+		//break;
+
+	//case 2:
+	
+		return subtracao(5,3);
+		//break;
+
+	//}
+
+	
+	//return 0;
+
+}
